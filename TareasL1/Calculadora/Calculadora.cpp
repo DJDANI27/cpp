2b6352defad6017d6d29c701755aa54d55278a10
Luis Moncada <luisdaniel123dj@gmail.com>
2022-06-01 20:32:02 -0600

    Tarea 1

---
+++ TareasL1/Calculadora/Calculadora.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char** argv) 
+{
+    int a = 0;
+    int b = 0;
+    int Resultado = 0;
+
+    cout <<"ingrese el valor de a: ";
+    cin >> a;
+
+cout << endl;
+
+    cout << "ingrese el valor de b :";
+    cin >> b;
+
+cout << endl;
+cout << "resultado de la suma: "; 
+Resultado = a + b;
+cout << Resultado;
+
+cout << endl;
+cout << "resultado de la resta: "; 
+Resultado = a - b;
+cout << Resultado;
+
+cout << endl;
+cout << "resultado de la multiplicacion: "; 
+Resultado = a * b;
+cout << Resultado;
+
+cout << endl;
+cout << "resultado de la division: "; 
+Resultado = a / b;
+cout << Resultado;
+
+}
+
